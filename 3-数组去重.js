a5a66a33eff60e7a50224ad992edc5a7ed8dc76f
webRambler <talent1203@163.com>
2021-11-09 13:16:24 +0800

    3-数组去重.js，三种方法：set；常规双重循环；第三种方法先利用快速排序对数组进行排序，然后再用双指针一前一后遍历数组，重复的删掉，降低了时间复杂度为O(nlogn)

--- 3-数组去重.js
+++ 3-数组去重.js
@@ -16,3 +16,42 @@ function unique(arr) {
 }
 
 // 法三：使用快速排序(O(nlogn))对数组进行升序排序，然后利用双指针(O(n))，一次循环，比较相邻两项的值是否相等，最后的时间复杂度为O(nlogn)
+Array.prototype.quickSort = function quickSort() {
+  const rec = (arr) => {
+    if (!arr.length) return []
+    const left = []
+    const right = []
+    const mid = arr[0]
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i] < mid) {
+        left.push(arr[i])
+      } else {
+        right.push(arr[i])
+      }
+    }
+    return [...rec(left), mid, ...rec(right)]
+  }
+  const nums = rec(this)
+  // 最后复制到原数组中
+  for (let i = 0; i < nums.length; i++) {
+    this[i] = nums[i]
+  }
+  return this
+}
+
+console.log(nums.quickSort());
+console.log(nums, 36)
+
+// 双指针（一前一后，相邻）
+let left = 0, right = 1  //(默认数组长度大于1，如果小于1则不用去重了)
+while(right < nums.length) {
+  if (nums[left] !== nums[right]) {
+    nums[left + 1] = nums[right]
+    left++
+    right++
+  } else {
+    right++
+  }
+}
+nums.length = left
+console.log(nums, 333)
