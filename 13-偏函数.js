6947a1d5be158349a250d00b145aba38ce398b2d
webRambler <talent1203@163.com>
2021-11-09 23:34:51 +0800

    feat：13-偏函数.js

---
+++ 13-偏函数.js
@@ -0,0 +1,13 @@
+// 什么是偏函数？偏函数就是将一个 n 参的函数转换成固定 x 参的函数，剩余参数（n - x）将在下次调用全部传入。
+function partial(fn) {
+  const args = [...arguments].slice(1)
+  return function () {
+    return fn(...args, ...arguments)
+  }
+}
+
+function add(a, b, c) {
+  return a + b + c
+}
+let partialAdd = partial(add, 1)
+partialAdd(2, 3)
