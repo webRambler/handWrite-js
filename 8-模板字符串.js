da98ab79f8e759795d3dcc9682a8c04bfce1f454
webRambler <talent1203@163.com>
2021-11-09 18:52:27 +0800

    feat：8-模板字符串.js

--- 8-模板字符串.js
+++ 8-模板字符串.js
@@ -1,11 +1,11 @@
 // 法一：replace
-// function render(templateStr, data) {
-//   const reg = /\{\{(\w+)\}\}/g
-//   const str = templateStr.replace(reg, function () {
-//     return data[arguments[1]]
-//   })
-//   return str
-// }
+function render(templateStr, data) {
+  const reg = /\{\{(\w+)\}\}/g
+  const str = templateStr.replace(reg, function () {
+    return data[arguments[1]]
+  })
+  return str
+}
 
 // 使用while不停的使用正则test也可以
 function render(templateStr, data) {
