fc0526cc9cfe58d2448d6ef1468cc7521c81bc19
webRambler <talent1203@163.com>
2021-11-14 18:42:23 +0800

    feat：findIndex

---
+++ 15-数组/findIndex.js
@@ -0,0 +1,16 @@
+Array.prototype.myFindIndex = function (cb) {
+  const arr = this
+  for (let i = 0; i < arr.length; i++) {
+    if (cb(arr[i], i, arr)) return i
+  }
+  return -1
+}
+
+const arr = [3, 2, 5, 6]
+
+const res = arr.myFindIndex((item, i, arr1) => {
+  console.log(item, i, arr1, 666)
+  return item > 4
+})
+
+console.log(res, 'res')
