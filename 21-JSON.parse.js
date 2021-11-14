b86704738c49fe01608a4c940b6eadcbe3325639
webRambler <talent1203@163.com>
2021-11-14 21:12:43 +0800

    feat：21-JSON.parse

--- 21-JSON.parse.js
+++ 21-JSON.parse.js
@@ -1,8 +1,15 @@
+// 法一：new Function，  return
 function parse(str) {
-  if (/^\d+\.?\d*$/.test(str)) return parseFloat(str)
+  return new Function('return ' + str)()
+}
 
-
+// 方法二：eval
+function parse(str) {
+  return eval('(' + str + ')')
 }
 
 console.log(JSON.parse("2.6"));
 console.log(parse('12.6'));
+
+console.log(JSON.parse('{"name": "Tom", "age": 23}'));
+console.log(parse('{"name": "Tom", "age": 23}'));
